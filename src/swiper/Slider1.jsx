14a76825dc0ac3083683de395a6e6578741062c8
anzilabdulhakkim <anzilabdulhakkim135@gmail.com>
2024-04-15 21:55:51 +0530

    added the sliders

---
+++ src/swiper/Slider1.jsx
@@ -0,0 +1,104 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Box, Image } from "@chakra-ui/react";
+import "swiper/swiper-bundle.css";
+import { Navigation, Pagination, A11y, Autoplay } from "swiper/modules";
+
+const Slider1 = () => {
+    return (
+    <Box width="95%" mx="auto" pr={25}>
+      <Swiper 
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        modules={[Navigation, Pagination, A11y, Autoplay]}
+        spaceBetween={10}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+      <SwiperSlide>
+        <Box h="230px" w="100%">
+          <Image
+            src="https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/fd78c56eb85bafd5.jpg?q=20"
+            alt=""
+            h="100%"
+            w="100%"
+            objectFit="cover"
+          />
+        </Box>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Box h="230px" w="100%">
+          <Image
+            src="https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/74a1461287a69078.jpg?q=20"
+            alt=""
+            h="100%"
+            w="100%"
+            objectFit="cover"
+          />
+        </Box>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Box h="230px" w="100%">
+          <Image
+            src="https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/ed4591126ff69acb.png?q=20"
+            alt=""
+            h="100%"
+            w="100%"
+            objectFit="cover"
+          />
+        </Box>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Box h="230px" w="100%">
+          <Image
+            src="https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/4d279cfffa536c76.png?q=20"
+            alt=""
+            h="100%"
+            w="100%"
+            objectFit="cover"
+          />
+        </Box>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Box h="230px" w="100%">
+          <Image
+            src="https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/07e1b1bafa69088c.jpg?q=20"
+            alt=""
+            h="100%"
+            w="100%"
+            objectFit="cover"
+          />
+        </Box>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Box h="230px" w="100%">
+          <Image
+            src="https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/9277c7d894d50bef.jpg?q=20"
+            alt=""
+            h="100%"
+            w="100%"
+            objectFit="cover"
+          />
+        </Box>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Box h="230px" w="100%">
+          <Image
+            src="https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/1686434dbad73524.jpg?q=20"
+            alt=""
+            h="100%"
+            w="100%"
+            objectFit="cover"
+          />
+        </Box>
+      </SwiperSlide>
+    </Swiper>
+    </Box>
+  );
+};
+
+export default Slider1;
---
+++ src/swiper/Slider2.jsx
@@ -0,0 +1,142 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Box, Image, Text } from "@chakra-ui/react";
+import { Navigation, A11y, } from "swiper/modules";
+import "swiper/swiper-bundle.css";
+
+const Slider2 = () => {
+  return (
+    <Swiper
+      className="swiper"
+      modules={[Navigation, A11y]}
+      spaceBetween={50}
+      slidesPerView={5}
+      navigation
+      pagination={{ clickable: true }}
+      onSlideChange={() => console.log("slide change")}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>
+        <Box textAlign="center">
+          <Image
+            src="https://rukminim2.flixcart.com/image/200/200/xif0q/mobile/u/h/7/edge-40-neo-payj0004in-motorola-original-imagtkezzam4qxfq.jpeg?q=70"
+            alt=""
+            width="150px"
+          />
+          <Text fontSize="md" fontWeight="bold" mt="2">
+            moto edge 40 neo
+          </Text>
+          <Text fontSize="sm" color="gray.500">
+            incl of offers
+          </Text>
+        </Box>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Box textAlign="center">
+          <Image
+            src="https://rukminim2.flixcart.com/image/200/200/xif0q/mobile/5/y/8/-original-imagtt4mhqrzjs9r.jpeg?q=70"
+            alt=""
+            width="170px"
+          />
+          <Text fontSize="md" fontWeight="bold" mt="2">
+            vivo t2 pro 5g
+          </Text>
+          <Text fontSize="sm" color="gray.500">
+            incl of offers
+          </Text>
+        </Box>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Box textAlign="center">
+          <Image
+            src="https://rukminim2.flixcart.com/image/200/200/xif0q/mobile/s/9/i/m6-pro-5g-mzb0eqjin-poco-original-imags3e7dazavyje.jpeg?q=70"
+            alt=""
+            width="130px"
+          />
+          <Text fontSize="md" fontWeight="bold" mt="2">
+            poco m6 pro
+          </Text>
+          <Text fontSize="sm" color="gray.500">
+            incl of offers
+          </Text>
+        </Box>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Box textAlign="center">
+          <Image
+            src="https://rukminim2.flixcart.com/image/200/200/xif0q/mobile/u/v/h/-original-imagxaqtzmqgtfen.jpeg?q=70"
+            alt=""
+            width="90px"
+          />
+          <Text fontSize="md" fontWeight="bold" mt="2">
+            realme 12 5G
+          </Text>
+          <Text fontSize="sm" color="gray.500">
+            incl of offers
+          </Text>
+        </Box>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Box textAlign="center">
+          <Image
+            src="https://rukminim2.flixcart.com/image/200/200/xif0q/mobile/n/z/4/-original-imagz5g9gyhmgusp.jpeg?q=70"
+            alt=""
+            width="120px"
+          />
+          <Text fontSize="md" fontWeight="bold" mt="2">
+            POCO X6 Neo 5G
+          </Text>
+          <Text fontSize="sm" color="gray.500">
+            incl of offers
+          </Text>
+        </Box>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Box textAlign="center">
+          <Image
+            src="https://rukminim2.flixcart.com/image/200/200/xif0q/mobile/d/3/g/-original-imagy2v5ggthbvfe.jpeg?q=70"
+            alt=""
+            width="140px"
+          />
+          <Text fontSize="md" fontWeight="bold" mt="2">
+            poco m6 5g
+          </Text>
+          <Text fontSize="sm" color="gray.500">
+            incl of offers
+          </Text>
+        </Box>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Box textAlign="center">
+          <Image
+            src="https://rukminim2.flixcart.com/image/200/200/xif0q/mobile/w/i/d/-original-imagwzrguaee4pz6.jpeg?q=70"
+            alt=""
+            width="96px"
+          />
+          <Text fontSize="md" fontWeight="bold" mt="2">
+            poco x6 pro
+          </Text>
+          <Text fontSize="sm" color="gray.500">
+            incl of offers
+          </Text>
+        </Box>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Box textAlign="center">
+          <Image
+            src="https://rukminim2.flixcart.com/image/200/200/xif0q/mobile/9/x/f/-original-imagxtnehfg5pcwt.jpeg?q=70"
+            alt=""
+            width="160px"
+          />
+          <Text fontSize="md" fontWeight="bold" mt="2">
+            iPhone 15
+          </Text>
+          <Text fontSize="sm" color="gray.500">
+            incl of offers
+          </Text>
+        </Box>
+      </SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default Slider2;
